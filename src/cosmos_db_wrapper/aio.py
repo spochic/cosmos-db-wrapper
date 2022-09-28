f98bd96462a738c3e1384f2c7bc91d8860c98bda
Sebastien Pochic <spochic@ellipse.la>
2022-09-28 16:18:17 -0700

    Move 'await' statements

--- src/cosmos_db_wrapper/aio.py
+++ src/cosmos_db_wrapper/aio.py
@@ -13,32 +13,40 @@ async def get_or_create_db(client: CosmosClient, database_name: str) -> Database
     try:
         logging.debug(
             F"get_or_create_db()-Trying to create database '{database_name}'")
-        database_proxy = client.create_database(id=database_name)
+        database_proxy = await client.create_database(id=database_name)
         logging.debug(F"get_or_create_db()-Database '{database_name}' created")
-    except exceptions.CosmosResourceExistsError:
-        database_proxy = client.get_database_client(database=database_name)
-        logging.debug(
-            F"get_or_create_db()-Database {database_name} already exists")
 
-    return await database_proxy
+    except exceptions.CosmosResourceExistsError:
+        logging.debug(
+            F"get_or_create_db()-Database '{database_name}' already exists")
+        database_proxy = await client.get_database_client(database=database_name)
+
+    finally:
+        logging.debug(
+            F"get_or_create_db()-Returning proxy to '{database_name}' database")
+        return database_proxy
 
 
 async def get_or_create_container(database_proxy: DatabaseProxy, container_name: str, partition_key_path: str) -> ContainerProxy:
     try:
         logging.debug(
             F"get_or_create_container()-Trying to create container '{container_name}'")
-        container_proxy = database_proxy.create_container(
+        container_proxy = await database_proxy.create_container(
             id=container_name, partition_key=PartitionKey(
                 path=partition_key_path)
         )
         logging.debug(
             F"get_or_create_container()-Container '{container_name}' created")
+
     except exceptions.CosmosResourceExistsError:
-        container_proxy = database_proxy.get_container_client(container_name)
         logging.debug(
             F"get_or_create_container()-Container '{container_name}' already exists")
+        container_proxy = await database_proxy.get_container_client(container_name)
 
-    return await container_proxy
+    finally:
+        logging.debug(
+            F"get_or_create_container()-Returning proxy to '{container_name}' container")
+        return container_proxy
 
 
 async def query_items(container_proxy: ContainerProxy, query_text: str):
